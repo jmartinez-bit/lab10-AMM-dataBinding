15d7c74f99ef4341ee2af7dda42673979ec268a0
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-05-10 20:20:11 -0500

    Agregar archivos de proyecto.

---
+++ DataBindingLab10/DataBindingLab10/ViewModels/MonkeysPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataBindingLab10.Models;
+
+namespace DataBindingLab10.ViewModels
+{
+    public class MonkeysPageViewModel : ViewModelBase
+    {
+        public IList<Monkey> Monkeys { get { return MonkeyData.Monkeys; } }
+
+        Monkey selectedMonkey;
+        public Monkey SelectedMonkey
+        {
+            get { return selectedMonkey; }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+
+}
